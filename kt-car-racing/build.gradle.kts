c9a0baa2e2a601d9d0ef019cdd23ac1bd1debfbe
gunkim <gunkim.dev@gmail.com>
2022-08-26 23:52:57 +0900

    :art: 코틀린 자동차 경주 프로젝트 생성

---
+++ kt-car-racing/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "io.github.gunkim"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
