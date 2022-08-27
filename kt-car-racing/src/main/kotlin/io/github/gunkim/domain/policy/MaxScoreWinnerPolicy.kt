16f30abd80181dfafa6072b2b963d895d8fbef9f
gunkim <gunkim.dev@gmail.com>
2022-08-27 22:04:03 +0900

    :recycle: 우승 정책 코드 리팩토링

--- kt-car-racing/src/main/kotlin/io/github/gunkim/domain/policy/MaxScoreWinnerPolicy.kt
+++ kt-car-racing/src/main/kotlin/io/github/gunkim/domain/policy/MaxScoreWinnerPolicy.kt
@@ -4,9 +4,8 @@ import io.github.gunkim.domain.car.Car
 
 class MaxScoreWinnerPolicy : WinnerPolicy {
     override fun winner(cars: List<Car>): List<Car> {
-        val maxForward: Int = cars.maxBy(Car::forward).forward
-        return cars
-            .filter { it.forward == maxForward }
-            .toList()
+        return cars.groupBy { it.forward }
+            .maxBy { it.key }
+            .value
     }
 }
