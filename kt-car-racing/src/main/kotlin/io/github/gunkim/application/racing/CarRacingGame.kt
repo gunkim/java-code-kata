f1ed5cf60de1c997a780524ac02da7a6fc13b11b
gunkim <gunkim.dev@gmail.com>
2022-08-29 23:55:06 +0900

    :recycle: 의존성 파라미터 변경

--- kt-car-racing/src/main/kotlin/io/github/gunkim/application/racing/CarRacingGame.kt
+++ kt-car-racing/src/main/kotlin/io/github/gunkim/application/racing/CarRacingGame.kt
@@ -1,8 +1,9 @@
 package io.github.gunkim.application.racing
 
 import io.github.gunkim.application.io.Input
-import io.github.gunkim.application.io.Mode
 import io.github.gunkim.application.io.Output
+import io.github.gunkim.application.io.console.ConsoleInput
+import io.github.gunkim.application.io.console.ConsoleOutput
 import io.github.gunkim.domain.car.CarRaceTrack
 import io.github.gunkim.domain.policy.MaxScoreWinnerPolicy
 import io.github.gunkim.domain.policy.MovePolicy
@@ -10,13 +11,11 @@ import io.github.gunkim.domain.policy.RandomMovePolicy
 import io.github.gunkim.domain.policy.WinnerPolicy
 
 class CarRacingGame(
-    mode: Mode,
+    private val input: Input,
+    private val output: Output,
     private val movePolicy: MovePolicy,
     private val winnerPolicy: WinnerPolicy,
 ) {
-    private val input: Input = mode.input
-    private val output: Output = mode.output
-
     fun run() {
         output.carNameInput()
         val application = CarRaceTrack.of(input.input(), movePolicy, winnerPolicy)
@@ -31,7 +30,8 @@ class CarRacingGame(
     companion object {
         fun default(): CarRacingGame {
             return CarRacingGame(
-                Mode.CONSOLE,
+                ConsoleInput(),
+                ConsoleOutput(),
                 RandomMovePolicy(),
                 MaxScoreWinnerPolicy()
             )
