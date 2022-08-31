cb75b977c22d1b73e1b0aa7468530c3c79c730da
gunkim <gunkim.dev@gmail.com>
2022-08-31 23:45:05 +0900

    :sparkles: 당첨 로또 구현

---
+++ kt-lotto/src/main/kotlin/io/github/gunkim/domain/WinningLotto.kt
@@ -0,0 +1,16 @@
+package io.github.gunkim.domain
+
+class WinningLotto(
+    private val lotto: Lotto,
+    private val bonusNumber: LottoNumber,
+) {
+    init {
+        require(!lotto.contains(bonusNumber)) {
+            "당첨 번호와 보너스 번호는 중복될 수 없습니다."
+        }
+    }
+
+    fun matches(lotto: Lotto): Pair<Int, Boolean> {
+        return lotto.match(this.lotto) to lotto.contains(bonusNumber)
+    }
+}
