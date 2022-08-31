f6735962720ca9359fef4d50831239afabbc30e3
gunkim <gunkim.dev@gmail.com>
2022-08-31 23:45:27 +0900

    :sparkles: 입출력 구현

---
+++ kt-lotto/src/main/kotlin/io/github/gunkim/application/io/console/ConsoleInput.kt
@@ -0,0 +1,12 @@
+package io.github.gunkim.application.io.console
+
+import io.github.gunkim.application.io.Input
+
+class ConsoleInput : Input {
+    override val money
+        get() = readln().toInt()
+    override val winningNumbers
+        get() = readLine() ?: throw IllegalArgumentException("당첨 번호는 입력되어야 합니다.")
+    override val bonusNumber
+        get() = readln().toInt()
+}
