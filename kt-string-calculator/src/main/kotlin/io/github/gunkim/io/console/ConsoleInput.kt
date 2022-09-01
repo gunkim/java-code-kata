a9a4c08876ef06a6780526b1cbd4fa44016da54d
gunkim <gunkim.dev@gmail.com>
2022-09-01 23:48:50 +0900

    :recycle: null일 경우 예외가 발생하도록 수정

--- kt-string-calculator/src/main/kotlin/io/github/gunkim/io/console/ConsoleInput.kt
+++ kt-string-calculator/src/main/kotlin/io/github/gunkim/io/console/ConsoleInput.kt
@@ -3,5 +3,5 @@ package io.github.gunkim.io.console
 import io.github.gunkim.io.Input
 
 class ConsoleInput : Input {
-    override fun read(): String = readLine()!!
+    override fun read(): String = readLine() ?: throw IllegalArgumentException("계산식은 필수 입력 값입니다.")
 }
