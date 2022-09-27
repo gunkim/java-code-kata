5c8c586379bbb5abe7c24756b31b7967e4b291c9
gunkim <gunkim.dev@gmail.com>
2022-09-27 23:57:29 +0900

    :sparkles: 위치 객체 구현 및 테스트

--- kt-car-racing/src/main/kotlin/io/github/gunkim/domain/car/vo/Forward.kt
+++ kt-car-racing/src/main/kotlin/io/github/gunkim/domain/car/vo/Forward.kt
@@ -1,9 +1,9 @@
 package io.github.gunkim.domain.car.vo
 
-data class Forward(val forward: Int = 0) {
+data class Forward(val value: Int = 0) {
     init {
-        require(forward >= 0) {
-            "위치는 음수일 수 없습니다."
-        }
+        require(value >= 0) { "위치는 음수일 수 없습니다." }
     }
+
+    operator fun plus(number: Int) = Forward(value + number)
 }
--- kt-car-racing/src/test/kotlin/io/github/gunkim/domain/car/vo/ForwardTest.kt
+++ kt-car-racing/src/test/kotlin/io/github/gunkim/domain/car/vo/ForwardTest.kt
@@ -1,12 +1,13 @@
 package io.github.gunkim.domain.car.vo
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ForwardTest {
     @Test
     fun `음수일 경우 인스턴스 생성이 되지 않는다`() {
-        assertThrows<IllegalArgumentException>
-        { Forward(-1) }
+        assertThrows<IllegalArgumentException> { Forward(-1) }
+            .apply { assertThat(message).isEqualTo("위치는 음수일 수 없습니다.") }
     }
 }
