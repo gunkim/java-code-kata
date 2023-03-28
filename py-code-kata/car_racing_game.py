468f9c736d810138751cf16cf7508cdbffeaf1f0
gunkim <gunkim.dev@gmail.com>
2023-03-28 17:06:59 +0900

    :recycle: dataclass로 리팩토링

--- py-code-kata/car_racing_game.py
+++ py-code-kata/car_racing_game.py
@@ -1,22 +1,29 @@
+from dataclasses import dataclass, field
 from random import randint, randrange
 from typing import Callable
 
 
+@dataclass
 class Car:
-    def __init__(self, name: str, speed: int):
-        self.name = name
-        self.speed = speed
-        self.position = 1
+    name: str
+    speed: int
+    position: int = field(default=1)
+
+    def __post_init__(self):
+        assert self.speed > 0, "speed must be greater than 0"
+        assert self.name is not None, "name must be not None"
 
     def move(self, get_random: Callable[[int, int], int]):
         distance: int = get_random(1, self.speed)
         self.position = self.position + distance
 
 
+@dataclass
 class RacingGame:
-    def __init__(self, cars: list[Car]):
-        assert len(cars) > 1, "cars must be minimum 2"
-        self.cars = cars
+    cars: list[Car]
+
+    def __post_init__(self):
+        assert len(self.cars) > 1, "cars must be minimum 2"
 
     def play(self, distances: int):
         for distance in range(distances):
@@ -26,15 +33,18 @@ class RacingGame:
                 print(f"{car.name}:{car.position * '-'}")
 
         max_pos: int = max(self.cars, key=lambda x: x.position).position
-        winner_names: list[str] = [car.name for car in self.cars if car.position == max_pos]
+        winner_names: list[str] = [car.name for car in self.cars if
+                                   car.position == max_pos]
         if len(winner_names) == 1:
             print(f"winner is {winner_names[0]}")
         else:
-            print(f"winners are {', '.join([name for name in sorted(winner_names)])}")
+            print(
+                f"winners are {', '.join([name for name in sorted(winner_names)])}")
 
 
 def input_cars() -> list[Car]:
-    return [Car(name, randrange(1, 10)) for name in input("Enter names separated by comma.\n").split(',')]
+    return [Car(name, randrange(1, 10)) for name in
+            input("Enter names separated by comma.\n").split(',')]
 
 
 def main():
