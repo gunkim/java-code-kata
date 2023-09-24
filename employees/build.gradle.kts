d988c22fb66c00956a0900663e5a9885b6b56582
gunkim <gunkim.dev@gmail.com>
2023-09-24 23:48:00 +0900

    :hammer: 애플리케이션 프로필 설정

--- employees/build.gradle.kts
+++ employees/build.gradle.kts
@@ -9,5 +9,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.h2database:h2")
+    implementation("com.mysql:mysql-connector-j:8.1.0")
 }
