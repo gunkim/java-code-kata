f2744484f0c4b796085db0d1610b459b77051749
gunkim <gunkim.dev@gmail.com>
2023-12-04 01:39:48 +0900

    :fire: 토끼 게임 프로젝트 삭제

--- rabbit-game/src/main/kotlin/io/github/gunkim/rabbit/domain/rabbit/policy/CreatePositionPolicy.kt
+++
@@ -1,7 +0,0 @@
-package io.github.gunkim.rabbit.domain.rabbit.policy
-
-import io.github.gunkim.rabbit.domain.Position
-
-fun interface CreatePositionPolicy {
-    fun position(): Position
-}
--- rabbit-game/src/main/kotlin/io/github/gunkim/rabbit/domain/rabbit/policy/CreateRandomDirectionPolicy.kt
+++
@@ -1,7 +0,0 @@
-package io.github.gunkim.rabbit.domain.rabbit.policy
-
-import io.github.gunkim.rabbit.domain.Direction
-
-class CreateRandomDirectionPolicy : CreateDirectionPolicy {
-    override fun direction() = Direction.random()
-}
