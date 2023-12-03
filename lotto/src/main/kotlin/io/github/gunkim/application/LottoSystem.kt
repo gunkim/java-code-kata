1c484e54ed12d85ee49f55396097f13197f05932
gunkim <gunkim.dev@gmail.com>
2023-12-04 01:40:03 +0900

    :art: 포메팅 수정

--- lotto/src/main/kotlin/io/github/gunkim/application/LottoSystem.kt
+++ lotto/src/main/kotlin/io/github/gunkim/application/LottoSystem.kt
@@ -4,10 +4,10 @@ import io.github.gunkim.application.io.Input
 import io.github.gunkim.application.io.Output
 import io.github.gunkim.application.io.console.ConsoleInput
 import io.github.gunkim.application.io.console.ConsoleOutput
+import io.github.gunkim.domain.CreateRandomNumberPolicy
 import io.github.gunkim.domain.Lotto
 import io.github.gunkim.domain.LottoMachine
 import io.github.gunkim.domain.LottoNumber
-import io.github.gunkim.domain.CreateRandomNumberPolicy
 import io.github.gunkim.domain.Rank
 import io.github.gunkim.domain.WinningLotto
 
