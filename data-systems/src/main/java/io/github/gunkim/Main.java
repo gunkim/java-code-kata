ba0151bfa053482d53fe685ace8da87ca1133490
gunkim <gunkim.dev@gmail.com>
2023-12-04 23:20:45 +0900

    :recycle: 잘못 커밋한 코드 롤백

--- data-systems/src/main/java/io/github/gunkim/Main.java
+++ data-systems/src/main/java/io/github/gunkim/Main.java
@@ -10,16 +10,14 @@ import java.util.Map;
 public class Main {
     private static final String SAVE_PATH = "./";
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         StorageFactory<Map<String, Object>> storageFactory = new HashIndexStorageFactory<>();
         Storage<Map<String, Object>> storage = storageFactory.createSimpleStorage(SAVE_PATH);
 
-//        storage.save("123456", Map.of(
-//                "name", "London",
-//                "attractions", List.of("Big Ben", "London Eye3")
-//        ));
-
-        Thread.sleep(2_000);
+        storage.save("123456", Map.of(
+                "name", "London",
+                "attractions", List.of("Big Ben", "London Eye3")
+        ));
 
         stopwatch(() -> {
             var data = storage.find("123456");
