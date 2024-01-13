8c0e7c75cfc2057d71f029987c627fa90f738ee4
gunkim <gunkim.dev@gmail.com>
2024-01-13 16:33:08 +0900

    :heavy_plus_sign: 프로젝트 기본 설정

--- notifications/build.gradle.kts
+++ notifications/build.gradle.kts
@@ -8,5 +8,9 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-docker-compose")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
     runtimeOnly("com.h2database:h2")
+    implementation("mysql:mysql-connector-java")
 }
