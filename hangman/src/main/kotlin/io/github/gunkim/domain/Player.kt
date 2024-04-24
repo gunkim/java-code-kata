fc28b1202a7527529894e601ab2e7166f4ae8566
gunkim <gunkim.dev@gmail.com>
2024-04-24 18:18:56 +0900

    정답을 맞출 경우에도 라이프가 깎이는 문제 해결

--- hangman/src/main/kotlin/io/github/gunkim/domain/Player.kt
+++ hangman/src/main/kotlin/io/github/gunkim/domain/Player.kt
@@ -1,17 +1,18 @@
 package io.github.gunkim.domain
 
+typealias LettersMap = Map<Int, Letter>
+
 data class Player(
     val problem: Word,
-    val letters: MutableMap<Int, Letter> = sortedMapOf()
+    val letters: LettersMap = sortedMapOf()
 ) {
     val isCorrect: Boolean
         get() = letters.size == problem.length
 
     fun hit(letter: Letter): Player {
         val index = problem.indexOf(letter)
-        if (index == -1) return this
-
-        val newLetters = letters.apply { this[index] = letter }
-        return copy(letters = newLetters)
+        return if (index != -1)
+            copy(letters = letters + (index to letter))
+        else this
     }
 }
