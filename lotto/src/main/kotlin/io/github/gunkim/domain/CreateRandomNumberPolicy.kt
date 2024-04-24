1b36eaaf19dbad7b576e921d5419cb1a5e17dfee
gunkim <gunkim.dev@gmail.com>
2024-04-24 18:25:06 +0900

    매직 넘버 추출을 통한 리팩토링

--- lotto/src/main/kotlin/io/github/gunkim/domain/CreateRandomNumberPolicy.kt
+++ lotto/src/main/kotlin/io/github/gunkim/domain/CreateRandomNumberPolicy.kt
@@ -3,11 +3,9 @@ package io.github.gunkim.domain
 import java.util.random.RandomGenerator
 
 class CreateRandomNumberPolicy : CreateNumberPolicy {
-    override fun create(): Int = randomGenerator.nextInt(MAXIMUM_LOTTO_NUMBER) + 1
+    override fun create(): Int = randomGenerator.nextInt(LottoNumber.MAX_NUMBER) + 1
 
     companion object {
-        private const val MAXIMUM_LOTTO_NUMBER = 45
-
         private val randomGenerator = RandomGenerator.getDefault()
     }
 }
--- lotto/src/main/kotlin/io/github/gunkim/domain/LottoNumber.kt
+++ lotto/src/main/kotlin/io/github/gunkim/domain/LottoNumber.kt
@@ -2,6 +2,11 @@ package io.github.gunkim.domain
 
 data class LottoNumber(val value: Int) {
     init {
-        require(value in 1..45) { "'$value'는 로또번호 범위 1 ~ 45 사이를 벗어납니다." }
+        require(value in MIN_NUMBER..MAX_NUMBER) { "'$value'는 로또번호 범위 $MIN_NUMBER ~ $MAX_NUMBER 사이를 벗어납니다." }
+    }
+
+    companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
     }
 }
