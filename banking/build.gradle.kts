9b4dd4b1743d1155279cf16c982679640d9141ae
gunkim <gunkim.dev@gmail.com>
2024-05-18 22:48:54 +0900

    뱅킹 카타 프로젝트 생성

---
+++ banking/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "io.github.gunkim"
+version = "2023.12.03"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.25.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
