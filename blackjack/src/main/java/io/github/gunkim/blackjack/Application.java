56c0d3e5e6a0161c6392288ffb42d2061222f90e
gunkim <gunkim.dev@gmail.com>
2024-05-26 19:49:08 +0900

    모듈 제거

--- blackjack/src/main/java/io/github/gunkim/blackjack/Application.java
+++
@@ -1,7 +0,0 @@
-package io.github.gunkim.blackjack;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- blackjack/src/main/java/io/github/gunkim/blackjack/domain/vo/CardSuit.java
+++
@@ -1,8 +0,0 @@
-package io.github.gunkim.blackjack.domain.vo;
-
-public enum CardSuit {
-    SPADES,
-    DIAMONDS,
-    HEARTS,
-    CLUBS
-}
