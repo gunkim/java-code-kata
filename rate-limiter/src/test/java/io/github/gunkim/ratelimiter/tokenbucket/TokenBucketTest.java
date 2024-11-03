893d16f86c53ff70b6b341e75965e0b1af437bd6
gunkim <gunkim.dev@gmail.com>
2024-11-03 16:20:09 +0900

    테스트 리팩토링

--- rate-limiter/src/test/java/io/github/gunkim/ratelimiter/tokenbucket/TokenBucketTest.java
+++ rate-limiter/src/test/java/io/github/gunkim/ratelimiter/tokenbucket/TokenBucketTest.java
@@ -1,5 +1,6 @@
 package io.github.gunkim.ratelimiter.tokenbucket;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,18 +14,23 @@ import static org.mockito.Mockito.times;
 
 @DisplayName("TokenBucket은")
 class TokenBucketTest {
+    private Runnable request;
+
+    @BeforeEach
+    void setUp() {
+        request = Mockito.mock(Runnable.class);
+    }
+
     @Test
-    void 토큰_수가_0이면_요청_메서드가_요청을_삭제한다() {
+    void 토큰이_없는_경우_요청이_무시된다() {
         var tokenBucket = new TokenBucket(0, 20_000);
-        Runnable request = Mockito.mock(Runnable.class);
         tokenBucket.request(request);
         Mockito.verify(request, never()).run();
     }
 
     @Test
-    void 토큰_수가_최대치일_때_요청_메소드는_요청을_처리한다() {
+    void 토큰이_최대치일_때_모든_요청이_처리된다() {
         var tokenBucket = new TokenBucket(10, 20_000);
-        Runnable request = Mockito.mock(Runnable.class);
         for (int i = 0; i < 10; i++) {
             tokenBucket.request(request);
         }
@@ -32,9 +38,8 @@ class TokenBucketTest {
     }
 
     @Test
-    void 토큰_수가_3이고_요청이_6개_들어오면_3개만_처리한다() {
+    void 토큰이_모두_사용된_후_나머지_요청이_무시된다() {
         var tokenBucket = new TokenBucket(3, 20_000);
-        Runnable request = Mockito.mock(Runnable.class);
         for (int i = 0; i < 6; i++) {
             tokenBucket.request(request);
         }
@@ -42,9 +47,8 @@ class TokenBucketTest {
     }
 
     @Test
-    void 단일_스레드_환경에서_토큰_버킷이_올바르게_작동한다() {
+    void 단일_스레드_환경에서_정상_작동한다() {
         var tokenBucket = new TokenBucket(5, 1000);
-        Runnable request = Mockito.mock(Runnable.class);
         for (int i = 0; i < 7; i++) {
             tokenBucket.request(request);
         }
@@ -52,9 +56,8 @@ class TokenBucketTest {
     }
 
     @Test
-    void 다중_스레드_환경에서_토큰_버킷이_올바르게_작동한다() throws InterruptedException {
+    void 다중_스레드_환경에서_정상_작동한다() throws InterruptedException {
         var tokenBucket = new TokenBucket(100, 5_000);
-        Runnable request = Mockito.mock(Runnable.class);
         ExecutorService taskExecutor = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 150; i++) {
             taskExecutor.execute(() -> tokenBucket.request(request));
