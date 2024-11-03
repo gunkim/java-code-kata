c840044792fa8ea8ce9749029feddd355fcd8c16
gunkim <gunkim.dev@gmail.com>
2024-11-03 23:05:55 +0900

    패키지 임포트 수정

--- rate-limiter/src/main/java/io/github/gunkim/ratelimiter/Application.java
+++ rate-limiter/src/main/java/io/github/gunkim/ratelimiter/Application.java
@@ -1,6 +1,7 @@
 package io.github.gunkim.ratelimiter;
 
-import io.github.gunkim.ratelimiter.tokenbucket.TokenBucket;
+
+import io.github.gunkim.ratelimiter.bucket.TokenBucket;
 
 public class Application {
     public static void main(String[] args) {
