d0da51b3b1ffd3e9ca2e5175c48d386c363331bc
gunkim <gunkim.dev@gmail.com>
2024-11-04 00:56:49 +0900

    고정 윈도우 카운터 주석 작성

--- rate-limiter/src/main/java/io/github/gunkim/ratelimiter/window/FixedWindowRateLimiter.java
+++ rate-limiter/src/main/java/io/github/gunkim/ratelimiter/window/FixedWindowRateLimiter.java
@@ -6,6 +6,18 @@ import java.time.Instant;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * ## 처리 제한 알고리즘, 고정 윈도우 카운터
+ * - 고정된 간격의 윈도 내의 요청은 지정된 임계치 만큼 처리한다.
+ * - 임계치를 넘어선 요청은 새 윈도가 열릴 때까지 버려진다.
+ * <p>
+ * 윈도 경계 부근에 순간적으로 많은 트래픽이 집중된다면 할당된 임계치보다 많은 요청이 처리될 수 있는 문제가 있다.
+ * 예를 들어,
+ * - 윈도 크기가 1분이고 임계치가 100이라고 가정할 때,
+ * - 12:00:30에 100개의 요청이 처리되고,
+ * - 12:01:00에 윈도가 새로 열리면서 다시 100개의 요청이 처리될 수 있다.
+ * - 이러한 경우 12:00:30에서 12:01:00 사이에 200개의 요청이 처리된 것이다.
+ */
 public class FixedWindowRateLimiter implements RateLimiter {
     private final int maxRequestsPerWindow;
     private final int windowDurationMillis;
